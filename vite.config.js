a4d165041053e8a07603d670cbc6b5027809999b
Gloria Lim <glorialimartt@gmail.com>
2024-05-07 09:46:30 -0400

    Remove backend server proxy

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,4 @@ export default defineConfig({
       },
     }),
   ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
