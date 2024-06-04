7616dab9b71ab86841a512713177185fd5560ffa
Gloria Lim <glorialimartt@gmail.com>
2024-06-03 22:16:03 -0400

    Remove useAuthenticated

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,21 +1,17 @@
 import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useChatData } from "../../hooks/useChatData"
 import IconSidebar from "../AgentMainPage/SidePanel/IconSidebar";
 function PrivateRoute({ children }) {
   useChatData();
 
-  const { isAuthenticated } = useContext(AuthContext);
-
-  return isAuthenticated ? (
+  return (
     <div className="app flex h-screen bg-base-200">
       <IconSidebar />
       <div className="content w-[100%] bg-base-200">{children}</div>
     </div>
-  ) : (
-    <Navigate to="/login" />
   );
 }
 
